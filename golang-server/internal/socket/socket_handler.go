b671c47431334538537bb4796b784a34ffc2c5ee
ivandohan <srgivando@gmail.com>
2024-02-26 23:48:43 +0700

    Creating method for user to join the room

--- golang-server/internal/socket/socket_handler.go
+++ golang-server/internal/socket/socket_handler.go
@@ -4,10 +4,21 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/gorilla/websocket"
 )
 
 type Handler struct {
 	hub *Hub
+}
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize: 1024,
+	WriteBufferSize: 1024,
+	CheckOrigin: func (r *http.Request) bool {
+		// origin := r.Header.Get("Origin")
+		// return origin == "http://localhost:3000"
+		return true
+	},
 }
 
 func NewHandler(h *Hub) *Handler {
@@ -35,4 +46,31 @@ func (h *Handler) CreateRoom(c *gin.Context) {
 	}
 
 	c.JSON(http.StatusOK, request)
+}
+
+func (h *Handler) JoinRoom(c *gin.Context) {
+	conn, err := upgrader.Upgrade(c.Writer, c.Request, nil)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	// path looks like /websocket/join-room/:roomId?userId=1&username=user
+	roomId := c.Param("roomId")
+	userId := c.Query("userId")
+	username := c.Query("username")
+
+	client := &Client{
+		Conn: conn,
+		Message: make(chan *Message, 10),
+		ID: userId,
+		RoomID: roomId,
+		Username: username,
+	}
+
+	message := &Message{
+		Content: "A new user has joined the room.",
+		RoomID: roomId,
+		Username: username,
+	}
 }
